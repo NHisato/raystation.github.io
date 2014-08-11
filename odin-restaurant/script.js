e318477b4291077e4c1fea52821f353f451a46b8
raystation-neolaptop <rayuen@gmail.com>
2014-08-10 19:49:24 -0700

    added in images

--- odin-restaurant/script.js
+++ odin-restaurant/script.js
@@ -1,21 +1,21 @@
 function getHome(){
 	$("#content").empty();
-	$("#content").append('<img class="scale" src="http://marcpro-strava.com/wordpress/wp-content/uploads/2012/05/PadThai.jpg">');
+	$("#content").append('<img class="scale" src="img/home-padthai.jpg">');
 	$("#content").append("<p>Come dine with us! We are located in sunny California. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum veritatis hic neque! Illo facilis nobis, nihil, earum, atque sed voluptatum deserunt obcaecati assumenda nulla in, animi autem qui magni saepe.</p>");	
 }
 
 function getMenu(){
 	$("#content").empty();
-	$("#content").append('<img class="scale" src="http://thecandiedviolets.files.wordpress.com/2010/07/mga_1355.jpg">');
+	$("#content").append('<img class="scale" src="img/menu-salad.jpg">');
 	$("#content").append("<h2>menu</h2>");
 	$("#content").append("<ul><li>Chicken</li><li>Broccoli</li><li>Beef</li><li>Soup de Jour</li><ul>");	
 }
 
 function getAbout(){
 	$("#content").empty();
-	$("#content").append('<img class="scale" src="http://4.bp.blogspot.com/-YoxdEQVGzEI/T506nRpRhyI/AAAAAAAAA8A/u_C60NIhtOE/s1600/DSC_0779.JPG">');
+	$("#content").append('<img class="scale" src="img/about-noodles.jpg">');
 		$("#content").append("<h2>about</h2>");
-	$("#content").append("<p>Founded in 1984, we are founded by a group of lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum veritatis hic neque! Illo facilis nobis, nihil, earum, atque sed voluptatum deserunt obcaecati assumenda nulla in, animi autem qui magni saepe.</p><img class='scale' src='http://www.myzticisle.com/images/lamesa_ca-map.jpg'>");
+	$("#content").append("<p>Founded in 1984, we are founded by a group of lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum veritatis hic neque! Illo facilis nobis, nihil, earum, atque sed voluptatum deserunt obcaecati assumenda nulla in, animi autem qui magni saepe.</p><img class='scale' src='img/about-map.jpg'>");
 }
 
 $(document).ready(function() {
