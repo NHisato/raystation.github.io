4c86e8a35186eb995ccedab2e595a3b6e39efc2a
raystation-neolaptop <rayuen@gmail.com>
2014-08-10 19:43:33 -0700

    fixed up the copy

--- odin-restaurant/script.js
+++ odin-restaurant/script.js
@@ -1,30 +1,29 @@
 function getHome(){
 	$("#content").empty();
-	$("#content").append('<img class="scale" src="http://placekitten.com/800/400">');
-	$("#content").append("<h2>Wok the Line</h2>");
-	$("#content").append("<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum veritatis hic neque! Illo facilis nobis, nihil, earum, atque sed voluptatum deserunt obcaecati assumenda nulla in, animi autem qui magni saepe.</p>");	
+	$("#content").append('<img class="scale" src="http://marcpro-strava.com/wordpress/wp-content/uploads/2012/05/PadThai.jpg">');
+	$("#content").append("<p>Come dine with us! We are located in sunny California. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum veritatis hic neque! Illo facilis nobis, nihil, earum, atque sed voluptatum deserunt obcaecati assumenda nulla in, animi autem qui magni saepe.</p>");	
+}
+
+function getMenu(){
+	$("#content").empty();
+	$("#content").append('<img class="scale" src="http://thecandiedviolets.files.wordpress.com/2010/07/mga_1355.jpg">');
+	$("#content").append("<h2>menu</h2>");
+	$("#content").append("<ul><li>Chicken</li><li>Broccoli</li><li>Beef</li><li>Soup de Jour</li><ul>");	
+}
+
+function getAbout(){
+	$("#content").empty();
+	$("#content").append('<img class="scale" src="http://4.bp.blogspot.com/-YoxdEQVGzEI/T506nRpRhyI/AAAAAAAAA8A/u_C60NIhtOE/s1600/DSC_0779.JPG">');
+		$("#content").append("<h2>about</h2>");
+	$("#content").append("<p>Founded in 1984, we are founded by a group of lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum veritatis hic neque! Illo facilis nobis, nihil, earum, atque sed voluptatum deserunt obcaecati assumenda nulla in, animi autem qui magni saepe.</p><img class='scale' src='http://www.myzticisle.com/images/lamesa_ca-map.jpg'>");
 }
 
 $(document).ready(function() {
 	getHome();
 
-	$("#home").click(function() {
-		getHome();
-  });
-
-	$("#about").click(function() {
-		$("#content").empty();
-		$("#content").append('<img class="scale" src="http://placekitten.com/500/200">');
-			$("#content").append("<h2>about</h2>");
-		$("#content").append("<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum veritatis hic neque! Illo facilis nobis, nihil, earum, atque sed voluptatum deserunt obcaecati assumenda nulla in, animi autem qui magni saepe.</p>");
-  });
-
-	$("#menu").click(function() {
-		$("#content").empty();
-		$("#content").append('<img class="scale" src="http://placekitten.com/700/200">');
-		$("#content").append("<h2>menu</h2>");
-		$("#content").append("<ul><li>Chicken</li><li>Broccoli</li><li>Beef</li><li>Soup de Jour</li><ul>");
-  });
+	$("#home").click(function() { getHome(); });
+	$("#menu").click(function() { getMenu(); });
+	$("#about").click(function() { getAbout(); });
 
 	$('ul.nav li').mouseenter(function() {
 		$(this).fadeTo('fast','0.5');
